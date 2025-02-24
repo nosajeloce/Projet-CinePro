118e7e50aaa658e110870fe0185240e8642d1702
Jason Nguyen1 <jnguyen1@dti.crosemont.quebec>
2025-02-24 16:23:09 -0500

    Fichiers de base

---
+++ modele/DAO/configs/ConfigBD.interface.php
@@ -0,0 +1,12 @@
+<?php	
+	/* Description : paramettres d'accès à la BD
+
+	*/
+	interface ConfigBD
+	{	
+		const BD_HOTE = "localhost";
+		const BD_UTILISATEUR = "root";
+		const BD_MOT_PASSE = "root";
+		const BD_NOM = "cinepro";    
+	}
+?>
